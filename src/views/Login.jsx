4ee2ece1698af6c1b747f83f275d9f38a4de162d
sepool7 <rajaee.sepideh@gmail.com>
2024-08-02 20:30:59 +0200

    first commit

---
+++ src/views/Login.jsx
@@ -0,0 +1,14 @@
+// App2 - views/Login.jsx
+
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <h2>Login Page</h2>
+      {/* Add your login form or login functionality here */}
+    </div>
+  );
+};
+
+export default Login;
